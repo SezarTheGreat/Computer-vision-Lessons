e1bb6da3d49a291ce0200fb30189ee97aac61c53
SezarTheGreat <jyotishmanjbbarman@gmail.com>
2025-10-21 23:53:27 +0530

    Countours Update

--- Lesson1/Countours.py
+++ Lesson1/Countours.py
@@ -15,11 +15,16 @@ ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 
 # Find and draw contours
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-img_contours = img.copy()
-cv2.drawContours(img_contours, contours, -1, (0, 255, 0), 2)
+# img_contours = img.copy()
+# cv2.drawContours(img_contours, contours, -1, (0, 255, 0), 2)
+
+for cnt in contours:
+    if cv2.contourArea(cnt) > 200:
+        x1,y1,w,h = cv2.boundingRect(cnt)
+        cv2.rectangle(img,(x1,y1),(x1+w,y1+h),(0,255,0),2)
 
 cv2.imshow("Original Image", img)
 cv2.imshow("Threshold Image", thresh)
-cv2.imshow("Contours", img_contours)
+# cv2.imshow("Contours", img_contours)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
